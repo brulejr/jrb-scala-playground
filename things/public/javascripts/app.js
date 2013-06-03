88d9531bf17e1dde8030b111839979d718ffba98
jbrule <brulejr@gmail.com>
2013-06-03 13:13:57 -0400

    Integrated ng-grid for report layout

--- things/public/javascripts/app.js
+++ things/public/javascripts/app.js
@@ -1,8 +1,20 @@
-var appModule = angular.module('things', [ 'restServices' ]);
+var appModule = angular.module('things', [ 'ngGrid', 'restServices' ]);
 
 appModule.controller('ThingCtrl', [ '$scope', 'ThingList',
 		function($scope, ThingList) {
 
 			$scope.things = ThingList.query();
+			$scope.gridOptions = {
+				data : 'things',
+				columnDefs : [ {
+					field : 'id',
+					displayName : 'UUID',
+					width : '300px'
+				}, {
+					field : 'name',
+					displayName : 'Name',
+					width : '200px'
+				} ]
+			};
 
 		} ]);
